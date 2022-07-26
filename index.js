6d181ef254f1b25fb9f3f8a8c9132f5c0fcaa768
Joseph Sutera <joesutera11@gmail.com>
2022-07-26 01:19:21 -0400

    commit tree 8c2f83143ae8afc9b4ec4970aa2c126cb5f16089

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 function addingEventListener() {
+ const attribute=document.getElementById("input").addEventListener("click", function(){ alert("Hello World!") });
+return attribute
 }
+addingEventListener()
